598920dfd85f7997dee9ee457cc93f7c33ca1c84
inna-dmytrenko <dmytinna@gmail.com>
2022-07-03 20:06:35 +0300

    feat: add mobie style

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,24 @@ const calculate = (...x) =>
         .concat(x, y)
         .reduce((a, b) => a + b, 0);
 };
+const iconMenu = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav');
+const navLink = document.querySelector('.nav-list');
+const bodyOverlay = document.querySelector('.overlayDiv');
+
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    iconMenu.classList.toggle('is-active');
+    navMenu.classList.toggle('open');
+    bodyOverlay.classList.toggle('overlay');
+    document.body.classList.toggle('hidden');
+  });
+}
+if (navLink) {
+  navLink.addEventListener('click', function (e) {
+    iconMenu.classList.remove('is-active');
+    navMenu.classList.remove('open');
+    bodyOverlay.classList.remove('overlay');
+    document.body.classList.remove('hidden');
+  });
+}
